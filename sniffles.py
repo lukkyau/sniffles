43875b675fd43d30b6871426c4daf77596a18b62
Jonathan <lukky.jono@gmail.com>
2019-03-30 15:36:15 +1100

    Only capture approved access points

--- sniffles.py
+++ sniffles.py
@@ -2,23 +2,28 @@ from access_points import *
 import datetime, sqlite3, time
 
 # Set location and sniff number
-location="pwc"
+location="library"
 countNum=5
-count=2
+count=1
+
+# Get approved list of MAC addresses
+approved=[]
+with open("approvedlist.txt") as file:
+    for line in file:
+        line = line.strip() #preprocess line
+        approved.append(line)
 
 # Create database
 conn=sqlite3.connect('signal_strengths.db')
 curr=conn.cursor()
-# curr.execute("DROP TABLE IF EXISTS signals")
-# curr.execute("DROP TABLE IF EXISTS lookups")
 curr.execute("CREATE TABLE IF NOT EXISTS signals(collected TEXT, location TEXT)")
 curr.execute("CREATE TABLE IF NOT EXISTS lookups(mac TEXT PRIMARY KEY, name TEXT, varNum INTEGER)")
 
 # Scan wifi and write to database
 lookupList=curr.execute("SELECT mac, name FROM lookups").fetchall()
-print(lookupList)
 
 def sniffMe():
+	# Retrieve wifi information (relies on airport)
 	wifi_scanner = get_scanner("en0")
 	sniffles=wifi_scanner.get_access_points()
 	collectionTime=datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S')
@@ -27,21 +32,27 @@ def sniffMe():
 	print("SNIFF",count)
 	if len(sniffles) > 0:
 		curr.execute("INSERT INTO signals(collected,location) VALUES (?,?)",(collectionTime,location))
+		print("Time: ",collectionTime)
+		print("Location: ",location)
 		for i in sniffles:
-			print("MAC:", i.bssid," Quality:",i.quality," Time:",collectionTime," Location:",location," SSID:",i.ssid )
-			if (i.bssid,i.ssid) in lookupList:
-				lookupNum=lookupList.index((i.bssid,i.ssid))+1
-				curr.execute("UPDATE signals SET AP{0}=(?) WHERE collected=(?)".format(lookupNum),(i.quality,collectionTime,))
-				curr.execute("UPDATE signals SET MAC{0}=(?) WHERE collected=(?)".format(lookupNum),(i.bssid,collectionTime,))
+			print("MAC:", i.bssid," Quality:",i.quality," SSID:",i.ssid )
+			if i.bssid in approved:
+				if (i.bssid,i.ssid) in lookupList:
+					lookupNum=lookupList.index((i.bssid,i.ssid))+1
+					curr.execute("UPDATE signals SET APMAC{0}=(?) WHERE collected=(?)".format(lookupNum),(i.bssid,collectionTime,))
+					curr.execute("UPDATE signals SET APSTRENGTH{0}=(?) WHERE collected=(?)".format(lookupNum),(i.quality,collectionTime,))
+					print("UPDATED - Approved wireless point.")
+				else:
+					lookupList.append((i.bssid,i.ssid))
+					lookupNum=len(lookupList)
+					curr.execute("INSERT INTO lookups VALUES (?,?,?)",(i.bssid,i.ssid,lookupNum))
+					curr.execute("ALTER TABLE signals ADD COLUMN APMAC{0} TEXT".format(lookupNum))
+					curr.execute("ALTER TABLE signals ADD COLUMN APSTRENGTH{0} INTEGER".format(lookupNum))
+					curr.execute("UPDATE signals SET APMAC{0}=(?) WHERE collected=(?)".format(lookupNum),(i.bssid,collectionTime,))
+					curr.execute("UPDATE signals SET APSTRENGTH{0}=(?) WHERE collected=(?)".format(lookupNum),(i.quality,collectionTime,))
+					print("ADDED - Approved wireless point.")
 			else:
-				print("NEW WIRELESS POINT ADDED")
-				lookupList.append((i.bssid,i.ssid))
-				lookupNum=len(lookupList)
-				curr.execute("INSERT INTO lookups VALUES (?,?,?)",(i.bssid,i.ssid,lookupNum))
-				curr.execute("ALTER TABLE signals ADD COLUMN MAC{0} TEXT".format(lookupNum))
-				curr.execute("ALTER TABLE signals ADD COLUMN AP{0} INTEGER".format(lookupNum))
-				curr.execute("UPDATE signals SET AP{0}=(?) WHERE collected=(?)".format(lookupNum),(i.quality,collectionTime,))
-				curr.execute("UPDATE signals SET MAC{0}=(?) WHERE collected=(?)".format(lookupNum),(i.bssid,collectionTime,))
+				print("IGNORED - Not approved wireless point.")
 
 	# Time delay
 	time.sleep(5)
@@ -53,7 +64,7 @@ while count < countNum:
 
 # Set nulls to zero for nearest neighbour algorithm
 for i in range (1,len(lookupList)+1):
-	curr.execute("UPDATE signals SET AP{0}=0 WHERE AP{0} IS NULL".format(i))
+	curr.execute("UPDATE signals SET APSTRENGTH{0}=0 WHERE APSTRENGTH{0} IS NULL".format(i))
 
 conn.commit()
 curr.close()
